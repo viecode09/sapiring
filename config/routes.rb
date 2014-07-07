d7004d62ab02026de17d5de8a26618bab1d0b4a2
Prelang Builder <builder@prelang.com>
2014-07-07 23:31:11 +0000

    Overrode the default Devise controllers in routes.rb to point to the new editable ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
